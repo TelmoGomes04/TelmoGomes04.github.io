6e8e4db760959d926ec8bc810adf591b88bbc023
Telmo Gomes <157317355+TelmoGomes04@users.noreply.github.com>
2024-04-19 14:18:09 +0100

    a

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
-import { ARButton } from "https://unpkg.com/three@0.126.0/examples/jsm/webxr/ARButton.js";
+import { ARButton } from "https://unpkg.com/three@0.128.0/examples/jsm/webxr/ARButton.js";
 import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
 
 
