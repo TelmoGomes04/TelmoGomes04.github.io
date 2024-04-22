02d067b4bc13e7e9d4c655e471b494897aa010c3
Telmo Gomes <157317355+TelmoGomes04@users.noreply.github.com>
2024-04-22 11:32:08 +0100

    full html test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
-import { ARButton } from "https://unpkg.com/three@0.163.0/examples/jsm/webxr/ARButton.js";
-import { GLTFLoader } from 'https://unpkg.com/three@0.163.0/examples/jsm/loaders/GLTFLoader.js';
+//import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
+//import { ARButton } from "https://unpkg.com/three@0.163.0/examples/jsm/webxr/ARButton.js";
+//import { GLTFLoader } from 'https://unpkg.com/three@0.163.0/examples/jsm/loaders/GLTFLoader.js';
 
 let container;
 let camera, scene, renderer;
